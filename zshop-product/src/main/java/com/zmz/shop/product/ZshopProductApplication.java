ef5c9f820db8b9af13d854009431b88b7eaa024c
IDen <zmzxdal@foxmail.com>
2021-10-25 19:38:24 +0800

    FEAT: 项目初始化效果测试-1025

--- zshop-product/src/main/java/com/zmz/shop/product/ZshopProductApplication.java
+++ zshop-product/src/main/java/com/zmz/shop/product/ZshopProductApplication.java
@@ -1,8 +1,10 @@
 package com.zmz.shop.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(basePackages = "com.zmz.shop.product.dao")
 @SpringBootApplication
 public class ZshopProductApplication {
 
--- zshop-product/src/test/java/com/zmz/shop/product/ZshopProductApplicationTests.java
+++ zshop-product/src/test/java/com/zmz/shop/product/ZshopProductApplicationTests.java
@@ -1,13 +1,32 @@
 package com.zmz.shop.product;
 
+import com.zmz.shop.product.entity.BrandEntity;
+import com.zmz.shop.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ZshopProductApplicationTests {
 
+    @Autowired
+    private BrandService brandService;
+
+
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("华为");
+        brandService.save(brandEntity);
+        System.err.println("测试代码。。。。保存成功");
+
+    }
+    @Test
+    void contextLoads2() {
+        BrandEntity brandEntity = brandService.getById(1L);
+        brandEntity.setDescript("华为品牌");
+        brandService.updateById(brandEntity);
+        System.err.println("测试代码。。。。保存成功");
     }
 
 }
